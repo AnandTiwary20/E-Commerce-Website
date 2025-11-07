4a326998dd12290983cec370f05756e0f6fa4ad9
AnandTiwary20 <anandtiwari0019@gmail.com>
2025-11-07 20:32:36 +0530

    Main jsx updated and cart is working fine

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
-import { render } from 'preact'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './app'
 import './index.css'
-import { App } from './app.jsx'
 
-render(<App />, document.getElementById('app'))
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
